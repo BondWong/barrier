86bfb4b59bf78d47b472d1e75f89e78fe7ba1210
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-10-01 00:48:55 -0700

    .

--- gtmpi_dissemination.c
+++ gtmpi_dissemination.c
@@ -9,7 +9,7 @@
     type flags = record
         myflags : array [0..1] of array [0..LogP - 1] of Boolean
 	partnerflags : array [0..1] of array [0..LogP - 1] of ^Boolean
-	
+
     processor private parity : integer := 0
     processor private sense : Boolean := true
     processor private localflags : ^flags
@@ -32,13 +32,42 @@
 	parity := 1 - parity
 */
 
+static int P, logP;
+
+int log2(int P) {
+	int res = 1;
+	int y = 2;
+	while (y < P) {
+		y *= 2;
+		res++;
+	}
+
+	return res;
+}
 
 void gtmpi_init(int num_threads){
-
+	P = num_threads;
+	logP = log2(P);
 }
 
 void gtmpi_barrier(){
+	int i;
+	MPI_Status status;
+	MPI_Comm_rank(MPI_COMM_WORLD, &i);
 
+	int k;
+	for (k = 0; k < logP; k++) {
+		int j = (i + (1 << k)) % P; // partner j
+		int l = (i - (1 << k) + P) % P; // l's partner is i
+		/*
+			do not need parity. partity's nature is to form a loop as the following:
+			[0, 0] => [1, 0] => [1, 1] (flip sense) => [0, 1] => [0, 0] (flip sense again)
+			what really happens is i signals its partner j and waits for l whose partner is i
+			therefore, i didn't implement parity here
+		 */
+		MPI_Send(NULL, 0, MPI_INT, j, 1, MPI_COMM_WORLD); // signals partner
+    MPI_Recv(NULL, 0, MPI_INT, l, 1, MPI_COMM_WORLD, &status); // wait for notification
+	}
 }
 
 void gtmpi_finalize(){
--- gtmpi_tournament.c
+++ gtmpi_tournament.c
@@ -19,14 +19,14 @@
 
     //initially
     //    rounds[i][k].flag = false for all i,k
-    //rounds[i][k].role = 
+    //rounds[i][k].role =
     //    winner if k > 0, i mod 2^k = 0, i + 2^(k-1) < P , and 2^k < P
     //    bye if k > 0, i mode 2^k = 0, and i + 2^(k-1) >= P
     //    loser if k > 0 and i mode 2^k = 2^(k-1)
     //    champion if k > 0, i = 0, and 2^k >= P
     //    dropout if k = 0
     //    unused otherwise; value immaterial
-    //rounds[i][k].opponent points to 
+    //rounds[i][k].opponent points to
     //    round[i-2^(k-1)][k].flag if rounds[i][k].role = loser
     //    round[i+2^(k-1)][k].flag if rounds[i][k].role = winner or champion
     //    unused otherwise; value immaterial
@@ -60,14 +60,118 @@
 	sense := not sense
 */
 
+// type round_t = record
+//     role : (winner, loser, bye, champion, dropout)
+//     opponent : ^Boolean
+//     flag : Boolean
+
+int log2(int P) {
+	int res = 1;
+	int y = 2;
+	while (y < P) {
+		y *= 2;
+		res++;
+	}
+
+	return res;
+}
+
+enum role{ WINNER, LOSER, BYE, CHAMPION, DROPOUT, UNUSED };
+
+struct round {
+  enum role role_type;
+  int opponent; // opponent is the vpid
+  // do not need flag here since there is not shared value changed
+};
+
+static int P, logP;
+struct round **rounds;
 
 void gtmpi_init(int num_threads){
+  P = num_threads;
+  logP = log2(P);
+  rounds = malloc(sizeof(struct round *) * P);
+  int i;
+  for (i = 0; i < P; i++) {
+    rounds[i] = malloc(sizeof(struct round) * (logP + 1)); // shared rounds : array [0..P-1][0..LogP] of round_t
+    int k;
+    for (k = 0; k < logP + 1; k++) {
+      //    winner if k > 0, i mod 2^k = 0, i + 2^(k-1) < P , and 2^k < P
+      if(k > 0 && i % (1 << k) == 0 && i + (1 << (k - 1)) < P && (1 << k) < P) rounds[i][k].role_type = WINNER;
+      //    bye if k > 0, i mode 2^k = 0, and i + 2^(k-1) >= P
+      else if(k > 0 && i % (1 << k) == 0 && i + (1 << (k -1)) >= P) rounds[i][k].role_type = BYE;
+      //    loser if k > 0 and i mode 2^k = 2^(k-1)
+      else if(k > 0 && (i % (1 << k)) == (1 << (k -1))) rounds[i][k].role_type = LOSER;
+      //    champion if k > 0, i = 0, and 2^k >= P
+      else if(k > 0 && i == 0 && (1 << k) >= P) rounds[i][k].role_type = CHAMPION;
+      //    dropout if k = 0
+      else if(k == 0) rounds[i][k].role_type = DROPOUT;
+      //    unused otherwise; value immaterial
+      else rounds[i][k].role_type = UNUSED;
 
+      // round[i-2^(k-1)][k].flag if rounds[i][k].role = loser
+      if (rounds[i][k].role_type == LOSER) rounds[i][k].opponent = i - (1 << (k - 1));
+      // round[i+2^(k-1)][k].flag if rounds[i][k].role = winner or champion
+      else if (rounds[i][k].role_type == WINNER || rounds[i][k].role_type == CHAMPION) rounds[i][k].opponent = i + (1 << (k - 1));
+      // unused otherwise; value immaterial
+      else rounds[i][k].opponent = -1;
+    }
+  }
 }
 
 void gtmpi_barrier(){
+	MPI_Status status;
+  int i;
+  MPI_Comm_rank(MPI_COMM_WORLD, &i);
+
+  int k;
+	for (k = 1; k <= logP; k++) {
+    // k starts with 1 guarantee role of dropout is impossible
+		switch(rounds[i][k].role_type) {
+	    // loser:
+      //   rounds[vpid][round].opponent^ :=  sense
+		  //   repeat until rounds[vpid][round].flag = sense
+			case LOSER:
+				MPI_Send(NULL, 0, MPI_INT, rounds[i][k].opponent, 1, MPI_COMM_WORLD);
+				MPI_Recv(NULL, 0, MPI_INT, rounds[i][k].opponent, 1, MPI_COMM_WORLD, &status);
+				break;
+      case BYE:
+        break;
+      // winner:
+      //   repeat until rounds[vpid][round].flag = sense
+			case WINNER:
+				MPI_Recv(NULL, 0, MPI_INT, rounds[i][k].opponent, 1, MPI_COMM_WORLD, &status);
+				break;
+      // champion:
+  	  //   repeat until rounds[vpid][round].flag = sense
+  		//   rounds[vpid][round].opponent^ := sense
+			case CHAMPION:
+				MPI_Recv(NULL, 0, MPI_INT, rounds[i][k].opponent, 1, MPI_COMM_WORLD, &status);
+				MPI_Send(NULL, 0, MPI_INT, rounds[i][k].opponent, 1, MPI_COMM_WORLD);
+				break;
+			default:
+				break;
+		}
+	}
+
+  // winner: rounds[vpid[round].opponent^ := sense
+  // bye: do nothing
+  // champion: impossible
+  // dropout: exit loop
+  for (k = logP - 1; k >= 0; k--) {
+    switch (rounds[i][k].role_type) {
+      case WINNER:
+        MPI_Send(NULL, 0, MPI_INT, rounds[i][k].opponent, 1, MPI_COMM_WORLD);
+        break;
+      default:
+        break;
+    }
+	}
+
+  // no need to flip sense for next barrier
+  // sense := not sense
 }
 
 void gtmpi_finalize(){
-
+  free(rounds);
 }
